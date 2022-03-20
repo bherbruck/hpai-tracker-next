c1e7608c19b5c8f7618f3f3c1b7c2a137bf65077
bherbruck <brennen.herbruck@gmail.com>
2022-03-20 19:45:29 -0400

    fix: map loading

--- src/components/map/HpaiMap.tsx
+++ src/components/map/HpaiMap.tsx
@@ -28,6 +28,10 @@ export const HpaiMap: FC<HpaiMapProps> = ({
     setLocation({ lat, lng, zoom })
   }
 
+  const invalidateSize = ({ target: map }: LeafletEvent) => {
+    map.invalidateSize()
+  }
+
   return (
     <MapContainer
       zoomControl={false}
@@ -35,7 +39,12 @@ export const HpaiMap: FC<HpaiMapProps> = ({
       zoom={location?.zoom ?? 5}
       className="h-full w-full"
     >
-      <MapEvents dragend={setMapLocation} zoomend={setMapLocation} />
+      <MapEvents
+        dragend={setMapLocation}
+        zoomend={setMapLocation}
+        load={invalidateSize}
+        layeradd={invalidateSize}
+      />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
