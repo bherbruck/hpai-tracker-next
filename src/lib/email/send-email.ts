30044706a1089309997586ebdff9f26842c22bc4
bherbruck <brennen.herbruck@gmail.com>
2022-03-21 14:03:50 -0400

    Create send-email.ts

---
+++ src/lib/email/send-email.ts
@@ -0,0 +1,25 @@
+import { createTransport, type SendMailOptions } from 'nodemailer'
+
+export async function sendEmail(
+  mailOptions: Omit<SendMailOptions, 'from'>,
+  userName: string = 'HPAI Tracker'
+) {
+  // maybe inject these with function arguments
+  const user = process.env.EMAIL_USER
+  const pass = process.env.EMAIL_PASS
+  const accessToken = process.env.EMAIL_API_KEY
+  try {
+    const transporter = createTransport({
+      service: 'gmail',
+      auth: { user, pass, accessToken },
+    })
+
+    const data = await transporter.sendMail({
+      from: `${userName} ${user}`,
+      ...mailOptions,
+    })
+    return { data }
+  } catch (error) {
+    return { error }
+  }
+}
