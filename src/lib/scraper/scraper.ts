aae51ce224f72f41804e17755f384ccc03b8ed2d
bherbruck <brennen.herbruck@gmail.com>
2022-03-21 11:45:13 -0400

    update the scraper to do full refresh

--- src/lib/scraper/scraper.ts
+++ src/lib/scraper/scraper.ts
@@ -5,20 +5,27 @@ export async function scrapeHpaiCases(url: string) {
   console.log(`fetching hpai cases...`)
 
   const hpaiCases = await fetchHpaiCases(url)
-
   const existingHpaiCaseCount = await prisma.hpaiCase.count()
-
   const newHpaiCases = hpaiCases.slice(existingHpaiCaseCount)
 
   console.log(`${newHpaiCases.length} new cases found`)
 
   if (newHpaiCases.length <= 0) return newHpaiCases
 
-  const { count: createdCount } = await prisma.hpaiCase.createMany({
-    data: newHpaiCases as any,
-  })
+  /**
+   * TODO:
+   * - create a new table for the import
+   * - run a diff on the new csv and the latest record in the import table
+   * - insert new rows
+   * - update existing rows
+   * - delete rows? (not sure if this is necessary)
+   */
+  // since the data source is not reliable and not indexed, we need to
+  // completely refresh the table to avoid duplicates
+  await prisma.hpaiCase.deleteMany()
+  await prisma.hpaiCase.createMany({ data: hpaiCases })
 
-  console.log(`${createdCount} new cases created`)
+  console.log(`Refrehed all hpai cases created`)
 
   return newHpaiCases
 }
