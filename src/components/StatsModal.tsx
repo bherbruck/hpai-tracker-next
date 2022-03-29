9ede3011f39e34ae982876b04eedc1e73564b1e7
bherbruck <brennen.herbruck@gmail.com>
2022-03-29 15:21:20 -0400

    add chart

--- src/components/StatsModal.tsx
+++ src/components/StatsModal.tsx
@@ -2,16 +2,47 @@ import { type ModalProps, Modal } from './Modal'
 import { FC, useEffect, useState } from 'react'
 import type { HpaiCaseGeometry, ClientSideHpaiCase, Stats } from '$lib/types'
 import { numberWithCommas } from '$lib/number-comma'
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+} from 'recharts'
 
 type StatsModalProps = ModalProps & {
   hpaiCases?: HpaiCaseGeometry[]
   stats?: Stats
 }
 
+type CumulativeHpaiCase = Pick<
+  ClientSideHpaiCase,
+  'dateConfirmed' | 'flockSize'
+>
+
 const sort = <T extends Record<any, any>>(
   arr: T[],
   compareFn: (a: T, b: T) => number
 ): T[] => [...arr].sort((a, b) => compareFn(a, b))
+
+const accumulateHpaiCases = (
+  hpaiCases: ClientSideHpaiCase[]
+): CumulativeHpaiCase[] =>
+  hpaiCases.reduce((acc, { dateConfirmed, flockSize }, index) => {
+    if (!acc.length) return [{ dateConfirmed, flockSize }]
+    const cumulativeFlockSize =
+      (acc[index - 1]?.flockSize ?? 0) + (flockSize ?? 0)
+    return [
+      ...acc,
+      {
+        dateConfirmed: new Date(dateConfirmed).toLocaleDateString(undefined, {
+          timeZone: 'utc',
+        }),
+        flockSize: cumulativeFlockSize,
+      },
+    ]
+  }, [] as CumulativeHpaiCase[])
 
 export const StatsModal: FC<StatsModalProps> = ({
   hpaiCases,
@@ -19,6 +50,9 @@ export const StatsModal: FC<StatsModalProps> = ({
   ...props
 }) => {
   const [flatCases, setFlatCases] = useState<ClientSideHpaiCase[]>([])
+  const [cumulativeCases, setCumulativeCases] = useState<CumulativeHpaiCase[]>(
+    []
+  )
 
   useEffect(() => {
     setFlatCases(
@@ -31,6 +65,20 @@ export const StatsModal: FC<StatsModalProps> = ({
     )
     return () => setFlatCases([])
   }, [hpaiCases])
+
+  useEffect(() => {
+    setCumulativeCases(
+      accumulateHpaiCases(
+        sort(flatCases, (a, b) => {
+          return (
+            new Date(a.dateConfirmed).valueOf() -
+            new Date(b.dateConfirmed).valueOf()
+          )
+        })
+      )
+    )
+    return () => setCumulativeCases([])
+  }, [flatCases])
 
   return (
     // TODO: make this mobile friendly
@@ -99,19 +147,54 @@ export const StatsModal: FC<StatsModalProps> = ({
                           .split(' ')[0]
                       }
                     </td>
-                    <td className="whitespace-pre-wrap">{state}</td>
-                    <td className="whitespace-pre-wrap">{county}</td>
-                    <td className="whitespace-pre-wrap">{flockType}</td>
-                    <td className="whitespace-pre-wrap">
-                      {numberWithCommas(flockSize as number)}
-                    </td>
+                    <td>{state}</td>
+                    <td>{county}</td>
+                    <td>{flockType}</td>
+                    <td>{numberWithCommas(flockSize as number)}</td>
                   </tr>
                 )
               )}
             </tbody>
           </table>
         </div>
+        <div className="flex-1">
+          {/* this should probably be its own component */}
+          <ResponsiveContainer>
+            <AreaChart data={cumulativeCases}>
+              <XAxis dataKey="dateConfirmed" />
+              <YAxis width={0} />
+              <Area
+                type="monotone"
+                dataKey="flockSize"
+                stroke="hsl(var(--a))"
+                fill="hsl(var(--a))"
+                fillOpacity={0.5}
+                strokeWidth={5}
+              />
+              {/* this should probably be its own component */}
+              <Tooltip
+                content={(props) =>
+                  props.payload &&
+                  props.payload.length > 0 && (
+                    <div className="rounded-lg bg-base-300 border-0 p-2 flex flex-col">
+                      <span className="font-bold text-sm text-base-content">
+                        {props.payload[0].payload.dateConfirmed}
+                      </span>
+                      <span className=" text-sm text-base-content">
+                        Birds Affected:
+                        <br />
+                        {numberWithCommas(props.payload[0].payload.flockSize)}
+                      </span>
+                    </div>
+                  )
+                }
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </Modal>
   )
 }
+
+export default StatsModal
