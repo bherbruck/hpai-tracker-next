41ef57bad9959c77c727ce92eafe3af8e3bf2855
bherbruck <brennen.herbruck@gmail.com>
2022-12-06 18:16:33 -0500

    scroll a few times before loading the data

--- scripts/playwright.ts
+++ scripts/playwright.ts
@@ -1,5 +1,12 @@
 import { chromium } from 'playwright'
 
+const URL =
+  'https://www.aphis.usda.gov/aphis/ourfocus/animalhealth/animal-disease-information/avian/avian-influenza/hpai-2022/2022-hpai-commercial-backyard-flocks'
+
+const TIMES_TO_SCROLL = 5
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 const main = async () => {
   const browser = await chromium.launch({
     headless: true,
@@ -7,14 +14,20 @@ const main = async () => {
   const context = await browser.newContext()
   const page = await context.newPage()
 
-  await page.goto(
-    'https://www.aphis.usda.gov/aphis/ourfocus/animalhealth/animal-disease-information/avian/avian-influenza/hpai-2022/2022-hpai-commercial-backyard-flocks'
-  )
+  await page.goto(URL)
 
-  // scroll down to the map
+  // scroll down to the map (it's not visible until you scroll down)
   await page.mouse.wheel(0, 100_000)
 
-  // wait for the map to load
+  // scroll up and down a few times to make sure the map loads
+  // there is a strange bug where the dashboard loads different "versions" data
+  for (let i = 0; i < TIMES_TO_SCROLL; i++) {
+    await page.mouse.wheel(0, -100)
+    await sleep(1000)
+    await page.mouse.wheel(0, 100)
+    await sleep(1000)
+  }
+
   await page
     .frameLocator('iframe')
     .locator(
@@ -22,14 +35,12 @@ const main = async () => {
     )
     .click()
 
-  // wait for the download button to load
   await page
     .frameLocator('iframe')
     .getByRole('toolbar', { name: 'Visualization controls' })
     .getByRole('button', { name: 'Download' })
     .click()
 
-  // wait for the data popup page to load
   const [dataPage] = await Promise.all([
     page.waitForEvent('popup'),
     page
@@ -39,7 +50,6 @@ const main = async () => {
       .click(),
   ])
 
-  // wait for the download to start
   const [download] = await Promise.all([
     page.waitForEvent('download'),
     dataPage.getByRole('button', { name: 'Download' }).click(),
