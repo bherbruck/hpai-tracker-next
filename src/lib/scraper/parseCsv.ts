7539aa7ee10457222b951616c3323aa67c42b3c4
bherbruck <brennen.herbruck@gmail.com>
2023-02-04 11:21:28 -0500

    handle new `dateReleased` format

--- src/lib/scraper/parseCsv.ts
+++ src/lib/scraper/parseCsv.ts
@@ -16,17 +16,24 @@ export function parseRawData(data: InputCsv): HpaiCaseInput {
   const flockSize = Number(
     data['Birds Affected'].toString().split(',').join('')
   )
-  const dateReleased =
-    data['Control Area Released'] === 'Active'
-      ? null
-      : new Date(data['Control Area Released'])
+
+  // if Control Area Released is 'NA', then dateReleased is dateConfirmed
+  // if Control Area Released is 'Active', then dateReleased is null
+  // if Control Area Released is a date, then dateReleased is that date
+  const dateReleased = () => {
+    if (data['Control Area Released'] === 'NA')
+      return new Date(data['Confirmed'])
+    else if (data['Control Area Released'] === 'Active') return null
+    else return new Date(data['Control Area Released'])
+  }
+
   return {
     dateConfirmed: new Date(data['Confirmed']),
     state: data.State,
     county: data['County Name'],
     flockType: data['Production'],
     flockSize: isNaN(flockSize) ? null : flockSize,
-    dateReleased: dateReleased,
+    dateReleased: dateReleased(),
     name: `${data['State']} ${data['Special Id']}`,
     pressReleaseUrl: null,
   }
