c33c6c59f268513581e0527c88bc16b23a529189
bherbruck <brennen.herbruck@gmail.com>
2025-01-28 21:44:22 -0500

    refactor: reorder email content in AlertEmail component

--- src/components/email/alert-email.tsx
+++ src/components/email/alert-email.tsx
@@ -15,14 +15,14 @@ export const AlertEmail: FC<Readonly<AlertEmailProps>> = ({
   <div className="h-full w-full">
     <h3>{hpaiCases.length} new HPAI cases</h3>
     <p>
-      You are receiving this email because you have subscribed to HPAI alerts
-      from the HPAI Alert website.
-    </p>
-    <p>
-      See them <a href={websiteUrl}>here</a>
+      See them on the map <a href={websiteUrl}>here</a>
     </p>
     <HpaiCaseTable hpaiCases={hpaiCases} />
     {/* unsubscribe */}
+    <p>
+      You are receiving this email because you have subscribed to HPAI alerts
+      from the HPAI Alert website.
+    </p>
     <p>
       To unsubscribe, click the following link:{' '}
       <a href={`${websiteUrl}/unsubscribe`}>Unsubscribe</a>
