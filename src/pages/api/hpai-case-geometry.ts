5d59fee4d3d7557598daa185d9ea8c315ccff66d
bherbruck <brennen.herbruck@gmail.com>
2025-01-28 20:44:17 -0500

    stage filtering cases to recent

--- src/pages/api/hpai-case-geometry.ts
+++ src/pages/api/hpai-case-geometry.ts
@@ -4,16 +4,36 @@ import { HpaiCase } from '@prisma/client'
 import { methodHandler } from '$lib/method-handler'
 
 const get: NextApiHandler = async (req, res) => {
+  const filterStart = new Date(new Date().setDate(new Date().getDate() - 7))
+
+  // only active or new cases (WOAH non-poultry are released same day as confirmed)
+  const filter = {
+    OR: [
+      {
+        dateReleased: null,
+      },
+      {
+        AND: [
+          { dateReleased: { equals: prisma.hpaiCase.fields.dateConfirmed } },
+          { dateReleased: { gte: filterStart } },
+        ],
+      },
+    ],
+  }
+
   const group = await prisma.hpaiCase.groupBy({
+    // where: filter,
     by: ['state', 'county'],
   })
-  const cases = await prisma.hpaiCase.findMany()
+  const cases = await prisma.hpaiCase.findMany({
+    // where: filter,
+  })
 
   const groupedCases = await Promise.all(
     group.map(async ({ state, county }) => {
       const countyCases = cases.filter(
         ({ state: caseState, county: caseCounty }) =>
-          state === caseState && county === caseCounty
+          state === caseState && county === caseCounty,
       )
       const geoJSON = (
         await prisma.countyGeometry.findUnique({
@@ -31,7 +51,7 @@ const get: NextApiHandler = async (req, res) => {
         cases: countyCases,
         geoJSON,
       }
-    })
+    }),
   )
 
   return res.json(groupedCases)
