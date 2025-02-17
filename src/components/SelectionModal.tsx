7021ac7e49baa3c815c254c07ebfc1a7c018b6e5
bherbruck <brennen.herbruck@gmail.com>
2025-02-17 13:51:58 -0500

    refactor: improve layout and styling of SelectionModal for better usability

--- src/components/SelectionModal.tsx
+++ src/components/SelectionModal.tsx
@@ -17,31 +17,43 @@ export const SelectionModal: FC<SelectionModalProps> = ({
 
   return (
     <Modal {...props} className="max-w-4xl">
-      <h3 className="font-bold text-lg pb-4">
-        {county} {state}
-      </h3>
-      <table className="table w-full h-full">
-        <thead>
-          <tr>
-            <th>Confirmed</th>
-            <th>Flock Type</th>
-            <th>Flock Size</th>
-            <th>Released</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cases.map(
-            ({ id, dateConfirmed, flockType, flockSize, dateReleased }) => (
-              <tr key={id}>
-                <td>{formatDate(dateConfirmed)}</td>
-                <td className="whitespace-pre-wrap">{flockType}</td>
-                <td>{numberWithCommas(flockSize as number)}</td>
-                <td>{dateReleased && formatDate(dateReleased)}</td>
-              </tr>
-            ),
-          )}
-        </tbody>
-      </table>
+      <div className="flex flex-col gap-4 w-full h-[75vh]">
+        <h3 className="font-bold text-lg pb-4">
+          {county} {state}
+        </h3>
+        <div className="flex-1 overflow-auto">
+          <div className="h-full">
+            <table className="table">
+              <thead className="bg-base-100 sticky top-0">
+                <tr>
+                  <th>Confirmed</th>
+                  <th>Flock Type</th>
+                  <th>Flock Size</th>
+                  <th>Released</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cases.map(
+                  ({
+                    id,
+                    dateConfirmed,
+                    flockType,
+                    flockSize,
+                    dateReleased,
+                  }) => (
+                    <tr key={id}>
+                      <td>{formatDate(dateConfirmed)}</td>
+                      <td className="whitespace-pre-wrap">{flockType}</td>
+                      <td>{numberWithCommas(flockSize as number)}</td>
+                      <td>{dateReleased && formatDate(dateReleased)}</td>
+                    </tr>
+                  ),
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </Modal>
   )
 }
